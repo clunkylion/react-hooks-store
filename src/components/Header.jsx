d307f372ceca0d968d6b438a17df84a436df0534
José Cortés <cote99salamanca@gmail.com>
2021-06-13 19:20:05 -0400

    se agregan iconos

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
-import '../styles/components/Header.css'
+import { Link } from 'react-router-dom';
+import '../styles/components/Header.css';
 
 const Header = () => (
   <div className="Header">
-    <h1 className="Header-title">PlatziConf Merch</h1>
-    <h1 className="Header-checkout">Checkout</h1>
+    <h1 className="Header-title">
+      <Link to="/">PlatziConf Merch</Link>
+    </h1>
+    <h1 className="Header-checkout">
+      <Link to="/checkout">
+        <i className="fas fa-shopping-basket" />
+      </Link>
+    </h1>
   </div>
 );
 
--- src/containers/Success.jsx
+++ src/containers/Success.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
+import '../styles/components/Success.css';
 
-const Success = () => <h1>Hola Success</h1>;
+const Success = () => (
+  <div className="Success">
+    <div className="Success-content">
+      <h2>José, gracias por tu compra</h2>
+      <span>Tú pedido llegan en 7 días</span>
+    </div>
+    <div className="Success-map">Google maps</div>
+  </div>
+);
 
 export default Success;
