3c1ab1b216491a1416e8c368abfd57241d97c755
José Cortés <cote99salamanca@gmail.com>
2021-06-13 17:55:47 -0400

    add header and footer and css

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../styles/components/Header.css'
+
+const Header = () => (
+  <div className="Header">
+    <h1 className="Header-title">PlatziConf Merch</h1>
+    <h1 className="Header-checkout">Checkout</h1>
+  </div>
+);
+
+export default Header;
